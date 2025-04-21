f12b1ce707271097a8d3c3fe63f43e5ad5e9752b
hyeonjiroh <sonamu6509@naver.com>
2025-04-21 07:10:10 +0000

    [feat, design] 커스텀 토스트 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
-import './globals.css';
-
 import type { Metadata } from 'next';
 import localFont from 'next/font/local';
+import './globals.css';
+import CustomToastContainer from '@/components/common/CustomToastContainer';
 
 const geistSans = localFont({
   src: './fonts/GeistVF.woff',
@@ -14,11 +14,10 @@ const geistMono = localFont({
   weight: '100 900',
 });
 
-//임시 주석 처리
-// export const metadata: Metadata = {
-//   title: 'Coworkers',
-//   description: '업무 배정 및 현황 공유 서비스',
-// };
+export const metadata: Metadata = {
+  title: 'Coworkers',
+  description: '업무 배정 및 현황 공유 서비스',
+};
 
 export default function RootLayout({
   children,
@@ -26,10 +25,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="ko">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <CustomToastContainer />
         {children}
       </body>
     </html>
---
+++ src/components/common/CustomToastContainer.tsx
@@ -0,0 +1,21 @@
+'use client';
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer, Zoom } from 'react-toastify';
+
+const CustomToastContainer = () => {
+  return (
+    <ToastContainer
+      limit={2}
+      position="top-center"
+      autoClose={3000}
+      newestOnTop={true}
+      hideProgressBar={false}
+      transition={Zoom}
+      draggable
+      pauseOnHover
+      pauseOnFocusLoss
+    />
+  );
+};
+
+export default CustomToastContainer;

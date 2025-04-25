781be8264654d242a2984c702aed6263680c9568
hyeonjiroh <sonamu6509@naver.com>
2025-04-25 12:52:19 +0000

    [design] toast: 위치, 디자인 변경

--- src/components/common/ToastStyle.tsx
+++ src/components/common/ToastStyle.tsx
@@ -7,7 +7,7 @@ const ToastStyle = () => {
   return (
     <ToastContainer
       limit={2}
-      position="bottom-center"
+      position="top-center"
       autoClose={3000}
       newestOnTop={true}
       hideProgressBar={false}

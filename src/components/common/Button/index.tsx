63beb6bdfcfca6db4eb4e509de82a6f98fe5d03f
hyeonjiroh <sonamu6509@naver.com>
2025-04-25 15:35:48 +0000

    chore: conflict 이슈로 임시 커밋

--- src/components/common/Button/index.tsx
+++ src/components/common/Button/index.tsx
@@ -1,3 +1,101 @@
-export default function Button() {
-  return <div>Button</div>;
+'use client';
+import { ReactNode } from 'react';
+import {
+  baseStyles,
+  disabledStyles,
+  radiusStyles,
+  sizeStyles,
+  variantStyles,
+  iconStyles,
+  ButtonVariant,
+  ButtonStyleType,
+} from '@/components/common/Button/style';
+import { twMerge } from 'tailwind-merge';
+import clsx from 'clsx';
+import Check from '@/assets/icons/check_white.svg';
+import CheckGreen from '@/assets/icons/check_green.svg';
+import Plus from '@/assets/icons/plus.svg';
+
+interface ButtonProps extends React.ComponentPropsWithoutRef<'button'> {
+  variant: ButtonVariant;
+  styleType: ButtonStyleType;
+  disabled?: boolean;
+  radius?: 'sm' | 'lg';
+  size?: 'sm' | 'md' | 'lg';
+  onClick?: () => void;
+  children: ReactNode;
+  startIcon?: 'check' | 'check_green' | 'plus';
 }
+
+const isValidStyleType = (
+  variant: ButtonVariant,
+  styleType: string
+): styleType is ButtonStyleType => {
+  return styleType in variantStyles[variant];
+};
+
+const Button = ({
+  variant = 'primary',
+  styleType = 'filled',
+  className,
+  disabled = false,
+  radius = 'lg',
+  size = 'md',
+  onClick,
+  children,
+  startIcon,
+  ...props
+}: ButtonProps) => {
+  if (!isValidStyleType(variant, styleType)) {
+    throw new Error(
+      `Invalid styleType "${styleType}" for variant "${variant}"`
+    );
+  }
+
+  const buttonStyles = twMerge(
+    clsx(
+      baseStyles,
+      className,
+      disabled
+        ? disabledStyles[styleType === 'outlined' ? 'outlined' : 'filled']
+        : variantStyles[variant][styleType],
+      radiusStyles[radius],
+      sizeStyles[size]
+    )
+  );
+
+  const renderIcon = () => {
+    if (!startIcon) return null;
+
+    const iconProps = {
+      width: 16,
+      height: 16,
+      className: iconStyles.start,
+    };
+
+    switch (startIcon) {
+      case 'check':
+        return <Check {...iconProps} />;
+      case 'check_green':
+        return <CheckGreen {...iconProps} />;
+      case 'plus':
+        return <Plus {...iconProps} />;
+      default:
+        return null;
+    }
+  };
+
+  return (
+    <button
+      className={buttonStyles}
+      onClick={onClick}
+      disabled={disabled}
+      {...props}
+    >
+      {renderIcon()}
+      {children}
+    </button>
+  );
+};
+
+export default Button;
--- src/components/common/Button/style.ts
+++ src/components/common/Button/style.ts
@@ -1,1 +1,65 @@
-// button component style
+export const baseStyles =
+  'box-border px-2 flex items-center justify-center transition-colors duration-100 cursor-pointer whitespace-nowrap overflow-hidden text-ellipsis';
+
+export type ButtonVariant = 'primary' | 'secondary' | 'floating';
+export type ButtonStyleType = {
+  primary: 'filled' | 'outlined' | 'danger';
+  secondary: 'filled' | 'outlined';
+  floating: 'default' | 'outlined' | 'transparent';
+}[ButtonVariant];
+
+interface ButtonStyles {
+  [key: string]: string;
+}
+
+const primaryStyles: ButtonStyles = {
+  filled:
+    'bg-green-700 text-lg-semibold text-white hover:bg-green-800 focus:bg-green-900 active:bg-green-900',
+  outlined:
+    'border text-lg-semibold bg-white border-green-700 text-green-700 hover:border-green-800 hover:text-green-800 focus:border-green-900 focus:text-green-900 active:border-green-900 active:text-green-900',
+  danger:
+    'bg-danger text-lg-semibold text-white hover:bg-red-700 focus:bg-red-800 active:bg-red-800',
+};
+
+const secondaryStyles: ButtonStyles = {
+  filled:
+    'bg-green-700 text-md-semibold text-white hover:bg-green-800 focus:bg-green-900 active:bg-green-900',
+  outlined:
+    'border text-md-semibold bg-transparent border-green-700 text-green-700 hover:border-green-800 hover:text-green-800 focus:border-green-900 focus:text-green-900 active:border-green-900 active:text-green-900',
+};
+
+const floatingStyles: ButtonStyles = {
+  default:
+    'bg-green-700 text-lg-semibold text-white hover:bg-green-800 focus:bg-green-900 active:bg-green-900 w-auto px-4',
+  outlined:
+    'border text-md-semibold bg-white border-green-700 text-green-700 w-auto px-4 [&_svg_path]:stroke-green-700 hover:border-green-800 hover:text-green-800 hover:[&_svg_path]:stroke-green-800 focus:border-green-900 focus:text-green-900 focus:[&_svg_path]:stroke-green-900 active:border-green-900 active:text-green-900 active:[&_svg_path]:stroke-green-900',
+  transparent:
+    'border text-md-semibold bg-transparent border-white text-white w-auto px-4 [&_svg_path]:stroke-white hover:border-green-800 hover:text-green-800 hover:[&_svg_path]:stroke-green-800 focus:border-green-900 focus:text-green-900 focus:[&_svg_path]:stroke-green-900 active:border-green-900 active:text-green-900 active:[&_svg_path]:stroke-green-900',
+};
+
+export const variantStyles = {
+  primary: primaryStyles,
+  secondary: secondaryStyles,
+  floating: floatingStyles,
+} as const;
+
+export const disabledStyles = {
+  filled: 'bg-slate-400 text-white cursor-not-allowed [&_svg]:text-white',
+  outlined:
+    'border border-slate-400 text-slate-400 cursor-not-allowed bg-white [&_svg_path]:stroke-slate-400',
+};
+
+export const radiusStyles = {
+  sm: 'rounded-xl',
+  lg: 'rounded-[40px]',
+};
+
+export const sizeStyles = {
+  sm: 'h-8',
+  md: 'h-10',
+  lg: 'h-[48px]',
+};
+
+export const iconStyles = {
+  start: 'mr-1',
+};

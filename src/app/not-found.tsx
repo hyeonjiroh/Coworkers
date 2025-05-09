1caae2683e8c140fcb75b159ff560212b683027a
hyeonjiroh <sonamu6509@naver.com>
2025-05-09 16:29:00 +0000

    feat: not-found 페이지 생성

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,3 +1,42 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import Button from '@/components/common/Button';
+
 export default function NotFound() {
-  return <div>404 Not Found</div>;
+  return (
+    <>
+      <div className="flex h-[calc(100vh-60px)] flex-col items-center justify-center overflow-hidden px-5 text-center text-white">
+        <div className="mb-10">
+          <Image
+            src="/image/error_404.png"
+            alt="404 Not Found"
+            width={810}
+            height={255}
+            className="mx-auto max-w-full opacity-90"
+          />
+        </div>
+
+        <h1 className="text-2xl-medium laptop:text-4xl-medium mb-5 font-bold">
+          페이지를 찾을 수 없습니다.
+        </h1>
+        <p className="text-md-medium laptop:text-lg-medium laptop:leading-6 mb-6 text-slate-500">
+          요청하신 페이지가 존재하지 않거나,
+          <br />
+          이동되었을 수 있습니다.
+        </p>
+
+        <Link href="/" className="w-[186px]">
+          <Button
+            variant="primary"
+            styleType="filled"
+            size="lg"
+            radius="sm"
+            className="!text-md-medium laptop:!text-lg-medium w-full"
+          >
+            홈으로 이동
+          </Button>
+        </Link>
+      </div>
+    </>
+  );
 }

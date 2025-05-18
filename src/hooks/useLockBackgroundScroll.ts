019b31ae029608c5a230204c90521f1a92394e91
hyeonjiroh <sonamu6509@naver.com>
2025-05-18 13:57:41 +0000

    [refactor] 모달 오픈 시 스크롤바 비활성화되며 아이템 밀림 이슈 해결 방식 gutter -> padding 변경

--- src/hooks/useLockBackgroundScroll.ts
+++ src/hooks/useLockBackgroundScroll.ts
@@ -3,10 +3,15 @@ import { useEffect } from 'react';
 export const useLockBackgroundScroll = (shouldLock: boolean) => {
   useEffect(() => {
     if (shouldLock) {
+      const scrollbarWidth =
+        window.innerWidth - document.documentElement.clientWidth;
+
       document.body.style.overflow = 'hidden';
+      document.body.style.paddingRight = `${scrollbarWidth}px`;
     }
     return () => {
       document.body.style.overflow = 'auto';
+      document.body.style.paddingRight = '';
     };
   }, [shouldLock]);
 };
